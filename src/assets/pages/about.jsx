cefba54c40395e5d2611e8ec36e65efbfe917ae1
Ikbal Maulana <86029687+ajobale@users.noreply.github.com>
2024-06-27 16:49:34 +0700

    rootlayout

---
+++ src/assets/pages/about.jsx
@@ -0,0 +1,5 @@
+function About() {
+  return <div>ini adalah halaman about</div>;
+}
+
+export default About;
--- src/assets/routers/index.jsx
+++ src/assets/routers/index.jsx
@@ -1,10 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
-
+import RootLayout from "../../layouts/RootLayout";
 import Home from "../pages/index";
+import About from "../pages/about";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
   },
 ]);
